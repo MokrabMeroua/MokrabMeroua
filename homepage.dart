1c956d98095b953da917ad3ae9911196c8010407
MokrabMeroua <155093492+MokrabMeroua@users.noreply.github.com>
2024-03-22 12:33:41 +0100

    Create homepage.dart

---
+++ homepage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil("login", (route) => false);
+              },
+              icon: Icon(Icons.exit_to_app))
+        ],
+      ),
+      body: ListView(
+        children: [],
+      ),
+    );
+  }
+}
